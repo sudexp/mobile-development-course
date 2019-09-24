a64308dee98a36775a89fef8aa4be9758bde976c
sudexp <sudexp@icloud.com>
2019-09-24 16:10:40 +0300

    pwa first test app created

---
+++ pwa_first_test_app/serviceworker.js
@@ -0,0 +1,20 @@
+var cacheName = 'myfirstpwa';
+var filesToCache = ['./', './index.html', './styles.css'];
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] install');
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] caching app');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] activate');
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => response || fetch(event.request))
+  );
+});
